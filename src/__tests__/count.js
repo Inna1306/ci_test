89981fe94990729f59b47b75611f0e5a84f07ca2
Inna1306 <inna742@gmail.com>
2026-06-14 22:22:50 +0300

    fixed count.js

--- src/__tests__/count.js
+++ src/__tests__/count.js
@@ -1,24 +1,28 @@
-import healthStatus from '../count';
+import healthStatus from '../count.js';
 
 const dataList = [
-    [
-        {
-            name: 'Маг', health: 90
-        }, 'healthy'
-    ],
-    [
-        {
-            name: 'Маг', health: 40
-        }, 'wounded'
-    ],
-    [
-        {
-            name: 'Маг', health: 10
-        }, 'critical'
-    ],
+  [
+    {
+      name: 'Маг', health: 90
+    }, 'healthy'
+  ],
+  [
+    {
+      name: 'Маг', health: 40
+    }, 'wounded'
+  ],
+  [
+    {
+      name: 'Маг', health: 10
+    }, 'critical'
+  ],
+  [
+    {
+      name: 'Маг', health: 0
+    }, 'critical'
+  ],
 ];
-
 test.each(dataList)('Testing healthStatus function with person %o', (person, expected) => {
-    const result = healthStatus(person);
-    expect(result).toBe(expected);
+  const result = healthStatus(person);
+  expect(result).toBe(expected);
 });
